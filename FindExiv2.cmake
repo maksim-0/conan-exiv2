4610c618035b865b397396687deb785ebed805fd
Luis Diaz Mas <piponazo@gmail.com>
2017-06-29 13:44:25 +0200

    Fix CMake Finder

--- FindExiv2.cmake
+++ FindExiv2.cmake
@@ -4,5 +4,4 @@ find_path(EXIV2_INCLUDE_DIRS NAMES exiv2/exiv2.hpp PATHS ${CONAN_INCLUDE_DIRS_EX
 find_library(EXIV2_LIBRARIES NAMES ${CONAN_LIBS_EXIV2} PATHS ${CONAN_LIB_DIRS_EXIV2})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Exiv2 DEFAULT_MSG REQUIRED_VARS
-    EXIV2_INCLUDE_DIRS EXIV2_LIBRARIES)
+find_package_handle_standard_args(Exiv2 DEFAULT_MSG EXIV2_INCLUDE_DIRS EXIV2_LIBRARIES)
