acb31902634c980bd068bfab4c473a34181bd64e
Luis Diaz Mas <piponazo@gmail.com>
2017-06-30 22:28:54 +0200

    Try to install latest CMake version on Linux (travis)

--- .travis/install.sh
+++ .travis/install.sh
@@ -17,6 +17,12 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     pyenv virtualenv 2.7.10 conan
     pyenv rehash
     pyenv activate conan
+else
+    mkdir $HOME/usr
+    export PATH="$HOME/usr/bin:$PATH"
+    wget https://cmake.org/files/v3.8/cmake-3.8.2-Linux-x86_64.sh
+    chmod +x cmake-3.8.2-Linux-x86_64.sh
+    ./cmake-3.8.2-Linux-x86_64.sh --prefix=$HOME/usr --exclude-subdir --skip-license
 fi
 
 pip install conan --upgrade
