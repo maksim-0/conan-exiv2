c7b594787f97dc17247174f56690abdb4e6d3276
Luis Diaz Mas <piponazo@gmail.com>
2017-06-30 22:15:13 +0200

    Fix problems to find Exiv2

--- FindExiv2.cmake
+++ FindExiv2.cmake
@@ -1,7 +1,6 @@
-# - Try to find the Exiv2 library
-
 find_path(EXIV2_INCLUDE_DIRS NAMES exiv2/exiv2.hpp PATHS ${CONAN_INCLUDE_DIRS_EXIV2})
 find_library(EXIV2_LIBRARIES NAMES ${CONAN_LIBS_EXIV2} PATHS ${CONAN_LIB_DIRS_EXIV2})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Exiv2 DEFAULT_MSG EXIV2_INCLUDE_DIRS EXIV2_LIBRARIES)
+find_package_handle_standard_args(Exiv2 DEFAULT_MSG 
+    EXIV2_INCLUDE_DIRS EXIV2_LIBRARIES)
